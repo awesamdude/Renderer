7fc9cb69d6f72e8ce00730ef9a204647a5e90350
awesamdude <awesamdudegaming@gmail.com>
2023-03-08 16:28:34 -0600

    Added timing for threading testing

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <random>
 #include <vector>
+#include <chrono>
 #include "Pixel.h"
 
 using namespace std;
-
+using namespace std::chrono;
 
 const int width = 255, height = 255;
 
@@ -19,15 +20,22 @@ int main() {
     img << width << " " << height << endl;
     img << "255" << endl;
 
+    auto start = high_resolution_clock::now();
     for (int y = 0; y < height; y++) {
         for (int x =0; x < width; x++) {
-            // Pixel* p = new Pixel();
-            // p->setRandomColor();
-            // img << p->writable();
-            Pixel* p = new Pixel(255, 0, 0);
+            Pixel* p = new Pixel();
+            p->setRandomColor();
             img << p->writable();
+            // Pixel* p = new Pixel(255, 0, 0);
+            // img << p->writable();
         }
     }
+    auto stop = high_resolution_clock::now();
+
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    cout << "Time taken: "<< duration.count() << " ms" << endl;
+
 
     system("open image.ppm");
 
